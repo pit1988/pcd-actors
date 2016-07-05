82bdbf54896e388b81911257e1a152077d2ba974
Pietro <pietro.gabelli@hotmail.com>
2016-07-05 15:37:14 +0200

    Implementation of ActorRef, ActorSystem and Message

---
+++ src/main/java/it/unipd/math/pcd/actors/implementation/MessageImpl.java
@@ -0,0 +1,62 @@
+/**
+ * The MIT License (MIT)
+ * <p/>
+ * Copyright (c) 2015 Riccardo Cardin
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * <p/>
+ */
+
+package it.unipd.math.pcd.actors.impl;
+
+import it.unipd.math.pcd.actors.AbsMessage;
+import it.unipd.math.pcd.actors.ActorRef;
+import it.unipd.math.pcd.actors.Message;
+
+/**
+ * Concrete implementation of AbsMessage.
+ * @author Pietro Gabelli
+ * @version 1.0
+ * @since 1.0
+ */
+
+public final class MessageImp<T extends Message> extends AbsMessage<T> {
+
+    public MessageImp(T message, ActorRef<T> sender) {
+        super(message, sender);
+    }
+    
+    /**
+     * Return the message object
+     * @return The message object
+     */
+    @Override
+    public Message getMessage() {
+        return super.message;
+    }
+
+    /**
+     * Return the sender object
+     * @return The sender object
+     */
+    @Override
+    public ActorRef getSender() {
+        return super.sender;
+    }
+}
